361e935dae0a35b414c1e3f7bfd85674aa965d94
Trisha Lewis <trishablewis@gmail.com>
2016-05-12 20:38:08 -0400

    prevented adding duplicates to library

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -61,9 +61,24 @@ class RecipesController < ApplicationController
   end
 
   def add_other_to_library
+    #find the user's recipe library and the recipe's id
     @library = Library.find_by(user_id: params[:user_id])
     @recipe_id = params[:recipe_id]
-    @recipe_library = RecipeLibrary.create(recipe_id: @recipe_id, library_id: @library.id)
+
+        #if not, add to library
+    library_check = false
+    #find all the recipes in the library
+    @recipe_lib = RecipeLibrary.where(library_id: @library.id)
+
+    #check to see if the recipe is already in the user's library
+    @recipe_lib.each do |recipe_lib|
+      if recipe_lib.recipe_id.to_i == @recipe_id.to_i
+        library_check = true
+        end
+    end
+    if library_check == false
+      @recipe_library = RecipeLibrary.create(recipe_id: @recipe_id, library_id: @library.id)
+    end
     redirect_to mylibrary_path(user_id: current_user.id)
   end
 
