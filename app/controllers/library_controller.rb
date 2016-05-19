98ea96abf12fe6af1fa398dcb1767b62d2023da8
Trisha Lewis <trishablewis@gmail.com>
2016-05-18 20:43:07 -0400

    styling

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,6 +1,6 @@
 class LibraryController < ApplicationController
   def mylibrary
-  	@user_id = params[:user_id]
-  	@mylibrary = Library.find_by(user_id: @user_id)
+  	@mylibrary = Library.find_by(user_id: params[:user_id])
+  	@recipes = @mylibrary.recipes.order(:name)
   end
 end
